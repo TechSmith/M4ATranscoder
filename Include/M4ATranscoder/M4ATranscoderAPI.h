e0dfebbc4f49ed30263e8351e5892709aa33f082
Steve Elgas <s.elgas@techsmith.com>
2015-02-09 09:29:05 -0500

    added an accessor for the list of available formats

--- Include/M4ATranscoder/M4ATranscoderAPI.h
+++ Include/M4ATranscoder/M4ATranscoderAPI.h
@@ -7,6 +7,9 @@
 #define WAVETOM4A_EXTERN extern "C"
 #endif
 
+#include <vector>
+#include "Mmreg.h"
+
 typedef void* WaveToM4AHandle;
 
 #define WAVETOM4A_SUCCESS               0
@@ -22,11 +25,13 @@ public:
 typedef int(*WAVETOM4ACREATE_FUNC)(WaveToM4AHandle* ppHandle);
 typedef int(*WAVETOM4AFREE_FUNC)(WaveToM4AHandle* ppHandle);
 typedef int(*WAVETOM4AINIT_FUNC)(WaveToM4AHandle pHandle, WCHAR* pstrInput, WCHAR* pstrOutput);
+typedef int(*WAVETOM4AGETFORMATS_FUNC)(WaveToM4AHandle pHandle, std::vector<WAVEFORMATEX>** pReturn);
 typedef int(*WAVETOM4A_FUNC)(WaveToM4AHandle pHandle, IM4AProgress* pProgress);
 
 WAVETOM4A_EXTERN int WaveToM4ACreate(WaveToM4AHandle* ppHandle);
 WAVETOM4A_EXTERN int WaveToM4AFree(WaveToM4AHandle* ppHandle);
 WAVETOM4A_EXTERN int WaveToM4AInit(WaveToM4AHandle pHandle, WCHAR* pstrInput, WCHAR* pstrOutput);
+WAVETOM4A_EXTERN int WaveToM4AGetFormats(WaveToM4AHandle pHandle, std::vector<WAVEFORMATEX>** pReturn);
 WAVETOM4A_EXTERN int WaveToM4A(WaveToM4AHandle pHandle, IM4AProgress* pProgress);
 
 #endif
--- WavToM4A/WavToM4A.cpp
+++ WavToM4A/WavToM4A.cpp
@@ -29,6 +29,14 @@ WAVETOM4A_EXTERN int WaveToM4AInit(WaveToM4AHandle pHandle, WCHAR* pstrInput, WC
    return WAVETOM4A_SUCCESS;
 }
 
+WAVETOM4A_EXTERN int WaveToM4AGetFormats(WaveToM4AHandle pHandle, std::vector<WAVEFORMATEX>** ppReturn)
+{
+   M4ATranscoder* pWav2M4A = (M4ATranscoder*)pHandle;
+   *ppReturn = pWav2M4A->GetOutputFormats();
+
+   return WAVETOM4A_SUCCESS;
+}
+
 WAVETOM4A_EXTERN int WaveToM4A(WaveToM4AHandle pHandle, IM4AProgress* pProgress)
 {
    M4ATranscoder* pWav2M4A = (M4ATranscoder*)pHandle;
